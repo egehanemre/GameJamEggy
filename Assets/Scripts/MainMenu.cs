571be3994325d6ce063869fceb9752eda5129e05
Egehan EMRE <16008122049@ogr.bozok.edu.tr>
2024-03-17 18:54:49 +0300

    main menu + settings gui

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoToSettings()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void GoToMain()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
